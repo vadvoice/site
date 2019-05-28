6925e14198cb3e4709f0112e8d97318e1c8e1b50
Vadym Shevchenko <vadym_shevchenko@Vadyms-MacBook-Pro.local>
2019-05-28 19:14:38 +0300

    added bunch of users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+const users = [
+  {name: 'John', age: 55},
+  {name: 'Lili', age: 24},
+  {name: 'Karl', age: 14},
+  {name: 'Piter', age: 34},
+]
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.json(users)
 });
 
 module.exports = router;
